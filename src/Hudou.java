1253089a46d09389f9019a46f1e9d653086eef5a
Hudou0420 <1598372655@qq.com>
2025-01-31 09:46:25 +0800

    Syncing with the remote base

--- src/Hudou.java
+++ src/Hudou.java
@@ -2,6 +2,7 @@ public class Hudou {
     public static void main(String[] args) {
         chatBot chatbot = new chatBot();
         chatbot.greeting();
-        chatBot.EndSession();
+        chatBot.echo();
+        chatBot.endSession();
     }
 }
--- src/chatBot.java
+++ src/chatBot.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class chatBot{
     public static void greeting(){
         String chatbotName = "Hudou";
@@ -11,10 +13,23 @@ public class chatBot{
         System.out.println(chatbotGreeting);
     }
 
-    public static void EndSession(){
+    public static void endSession(){
         String chatbotExit =  "\n-------------------------------------------\n" +
-                "Bye. Hope to see you again soon!\n\n" +
+                "Bye. Hope to see you again soon!\n" +
                 "-------------------------------------------\n";
         System.out.println(chatbotExit);
+    }
+
+    public static void echo(){
+        String user = null;
+        do {
+            Scanner cin = new Scanner(System.in);
+            user = cin.nextLine();
+            if (!user.equals("bye")) {
+                System.out.println("------------------------------------------");
+                System.out.println("You said: " + user);
+                System.out.println("------------------------------------------");
+            }
+        } while(!user.equals("bye"));
     }
 }
